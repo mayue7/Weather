f4e4677d75cb764b5774631dcf2a5556e79e2626
Mayue Jiang <mia.mayuejiang@gmail.com>
2023-11-25 10:15:47 +1100

    remove useless folder

--- obj/Debug/net6.0/WeatherApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/WeatherApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1ebc586ef1907f0f51167859fcb6b46dfeafb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05ebdb7468f649eb4198e35ef0d09169f2b504b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
